6c9bfb678075150dc80f82a68daa8aafdaeb9bdd
neggazi963 <neggazimohcen215@gmail.com>
2025-11-11 17:13:28 +0100

    Change filtered process name from 'pcb_info' to 'pcb_monitor'

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "pcb_info";
+static const char* process_to_filter = "pcb_monitor";
 
 /*
  * Get a directory name given a DIR* handle
